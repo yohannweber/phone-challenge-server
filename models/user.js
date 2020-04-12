1acebedd805dfd1868acbe4535ff179920d4eb63
YohannWEBER <yohann.weber@gmail.com>
2020-04-13 00:31:13 +0200

    Mise à jour des models et routes + upgrade des packages

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    externalId: String,
+    name: String,
+    calledUser: String,
+    date: Date,
+    comment: String
+});
+
+module.exports = mongoose.model('users', userSchema );
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../models/user')
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({users: [{name: 'Timmy'}]});
+router.get('/', (req, resp) => {
+  User.find({ }, (error, users) => {
+      if (error)
+          resp.status('404').send(error);
+      else
+          resp.send(users);
+  });
+});
+
+/* POST user */
+router.post('/', (req, resp) => {
+  let user = new User({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      calledUser: req.body.calledUser,
+      comment: req.body.comment,
+  });
+  user.validate(err => {
+      if (err)
+          console.log("err :" + err)
+      else
+          user.save()
+  });
+  resp.send(user);
 });
 
 module.exports = router;
